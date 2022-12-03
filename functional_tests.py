103666cbf4f19cd9e6e8f617154a59d86caa7ff4
James <jreinhardt@altusassessments.com>
2022-12-03 05:14:59 -0800

    First FT specced out in comments and not uses unnittests.
     Please enter the commit message for your changes. Lines starting

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,35 @@
 from selenium import webdriver
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+	
+    def tearDown(self):
+        self.browser.quit()
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self):
 
-assert 'Django' in browser.title
+        # Edith has heard about a cool new online to-do app.  She goes 
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+	# She is invited to enter a to-do item straight away
+
+	# She types "Buy peacock feathers" into a text box (Edith's hobby
+	# is tying fly-fishing lures
+
+	# When she hits enter, the page updats and now the page lists
+	# 1: buy peacock feathers to make fly
+
+	# The page updates again and shows both items on her list
+
+	# Edith wonders if the site will remember her list.  She sees a unique 
+	# Url has been generated for her to return to.
+
+	# She visits the url and sees her list is still there.
+if __name__ == '__main__':
+    unittest.main()
