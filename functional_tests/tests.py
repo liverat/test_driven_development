1c80a46a85ab9f7f645d7fc5197bbf4e825261f5
James <jreinhardt@altusassessments.com>
2022-12-06 22:34:39 -0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,19 +1,26 @@
 from selenium import webdriver
+from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 	
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app.  She goes 
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -28,16 +35,24 @@ class NewVisitorTest(unittest.TestCase):
          )
 
         inputbox.send_keys('Buy peacock feathers')
-
+    
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -53,5 +68,3 @@ class NewVisitorTest(unittest.TestCase):
 
 	    # She visits the url and sees her list is still there.
         self.fail('Finish the test!')
-if __name__ == '__main__':
-    unittest.main()
